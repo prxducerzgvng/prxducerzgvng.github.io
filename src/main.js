6a55a97e907382c072dca293548d5ebcd224bcbf
Soli Tsaagane <soli_rsa@outlook.com>
2023-06-24 15:33:59 +0200

    test refactor

--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
-import schema from "./schema.json" assert { type: "json" };
-
 const app = document.querySelector("#app");
 
 const main = () => {
-  app.innerHTML = `<section id="about">
-    <h2>About Us</h2>
-    <p>A Music Group (Recording Company & Artist Collective) founded and operated in Ekurhuleni, South Africa. Home to Buddha, Siiyam, Sean808 & Soli-RSA</p>
-  </section>`;
+
 };
 
 main();
